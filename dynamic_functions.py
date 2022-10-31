a4f162d729ec7852ca01a9da2ded24ed26d3b36a
Monica Pena <113930652+monica-ap@users.noreply.github.com>
2022-10-31 17:25:23 +0000

    learning the difference between pass and return false

---
+++ dynamic_functions.py
@@ -0,0 +1,31 @@
+def check_3Digits(list1):
+#  return list1 in range(100,1000)
+#lets add all the 3 digit numbers into their own list
+  three_digit_list=[]
+  for n in list1:
+    if n in range(100,1000):
+      three_digit_list.append(n)
+      # return True
+    else:
+      pass
+
+      return False
+
+#pass
+  
+########################################################################################################################
+# Dynamic Functions Practice #1
+# Create a function (all_positives) that returns True if all the values in a list are positive, and False if at least one of the values is negative. Create a list named numbers with positive and negative values.
+
+# Don't call the function, you just need to define it.
+
+
+########################################################################################################################
+# Dynamic Functions Practice #2
+# Create a function (sum_less) that adds the numbers of a list (stored in the variable numbers) as long as they are greater than 0 and less than 1000, and returns the result of said sum.
+
+
+
+########################################################################################################################
+# Dynamic Functions Practice #3
+# Create a function (count_even) that counts the number of even numbers that exist in a list (numbers), and returns the result of said count.
---
+++ main.py
@@ -0,0 +1,11 @@
+# from methods_help import method_help
+# from functions import function
+# from returnStuff import returnS
+from dynamic_functions import check_3Digits
+
+
+# method_help()
+# function()
+# returnS()
+result = check_3Digits([55,99,600,])
+print(result)
